92cc366d5d07b164f7a718642384e7c678e5152b
gershomlapaix <nsengiyumv33@gmail.com>
2023-09-04 11:54:25 +0200

    new configurations

--- Models/Movie.cs
+++
@@ -1,8 +0,0 @@
-namespace GsdsAuth.Models{
-    public class Movie{
-        public int Id {get; set;}
-        public string Title {get; set;}
-        public string Description {get; set;}
-        public float Rating {get; set;}
-    }
-}
